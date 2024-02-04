6b0301f343eab0e1abcef97e5042413815616e86
oskcarsv <oskcarqsv@gmail.com>
2024-02-03 22:26:51 -0600

    add javascript code to funcionality login

---
+++ assets/scripts/script-login.js
@@ -0,0 +1,25 @@
+const form = document.querySelector(".form");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const errorElement = document.createElement("p");
+errorElement.classList.add("error-message");
+
+const defaultUser = {
+  email: "contacly@example.com",
+  password: "123"
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const enteredEmail = emailInput.value;
+  const enteredPassword = passwordInput.value;
+
+  if (enteredEmail === defaultUser.email && enteredPassword === defaultUser.password) {
+    window.location.href = "contact.html";
+  } else {
+
+    form.appendChild(errorElement);
+    errorElement.textContent = "Credenciales incorrectas. Por favor, inténtalo de nuevo.";
+  }
+});
