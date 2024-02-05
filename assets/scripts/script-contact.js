8e94761814618accd35416bc05cbe08821203180
oskcarsv <oskcarqsv@gmail.com>
2024-02-04 19:49:32 -0600

    finished contacts page

--- assets/scripts/script-contact.js
+++ assets/scripts/script-contact.js
@@ -7,63 +7,38 @@ sidebarButton.onclick = function () {
 };
 
 
-// Contact cards
-const contacts = [
-    {
-        "name": "Oscar",
-        "lastName": "José",
-        "address": "Zona 10, Guatemala",
-        "email": "oscar@example.com",
-        "birthDate": "22/08/2006",
-        "phoneNumber": "+502 43568765"
-
-    },
-    {
-        "name": "Valerie",
-        "lastName": "Pacay",
-        "address": "Zona 9, Guatemala",
-        "email": "valerie@example.com",
-        "birthDate": "22/09/2007",
-        "phoneNumber": "+502 34567834"
-    },
-    {
-        "name": "Ottoniel",
-        "lastName": "Menéndez",
-        "address": "Zona 7, Guatemal",
-        "email": "ottoniel@example.com",
-        "birthDate": "28/10/2006",
-        "phoneNumber": "+502 09784576"
-    }
-    ,
-    {
-        "name": "Angel",
-        "lastName": "Méndez",
-        "address": "Zona 3, Guatemal",
-        "email": "angel@example.com",
-        "birthDate": "07-12-2005",
-        "phoneNumber": "+502 45764312"
-    },
-    {
-        "name": "José",
-        "lastName": "Soto",
-        "address": "Zona 1, Guatemal",
-        "email": "jose@example.com",
-        "birthDate": "16/11/2006",
-        "phoneNumber": "+502 12345678"
-    }
-];
-
 
 /* Contacto 1*/
-const contactoDiv = document.getElementById("contacto1");
+const contactoDiv = document.getElementById("contacto");
 
 contactoDiv.addEventListener("click", function () {
     window.location.href = "/assets/pages/contactDetail.html";
 });
 
 /* Contacto 2*/
+const contactoDiv1 = document.getElementById("contacto1");
+
+contactoDiv1.addEventListener("click", function () {
+    window.location.href = "/assets/pages/contactDetail1.html";
+});
+
+/* Contacto 3*/
 const contactoDiv2 = document.getElementById("contacto2");
 
 contactoDiv2.addEventListener("click", function () {
     window.location.href = "/assets/pages/contactDetail2.html";
+});
+
+/* Contacto 4*/
+const contactoDiv3 = document.getElementById("contacto3");
+
+contactoDiv3.addEventListener("click", function () {
+    window.location.href = "/assets/pages/contactDetail3.html";
+});
+
+/* Contacto 5*/
+const contactoDiv4 = document.getElementById("contacto4");
+
+contactoDiv4.addEventListener("click", function () {
+    window.location.href = "/assets/pages/contactDetail4.html";
 });
